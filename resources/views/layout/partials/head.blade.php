64903f6628543ae69af46dc0d47377af2e0dcddd
remD <remydailey@gmail.com>
2019-05-15 20:29:21 +0100

    Basic Structure

---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Clean Blog - Start Bootstrap Theme</title>
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<!-- Custom fonts for this template -->
+<link href="{{ URL::asset('css/fonts/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+<link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+<!-- Custom styles for this template -->
+<link href="{{ URL::asset('css/clean-blog.css') }}" rel="stylesheet" type='text/css'>
